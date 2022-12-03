75f8a07905155aabc5056f468bb8b68dd89ebb15
Spedon <RealSped0n@outlook.com>
2022-12-03 09:42:41 +0800

    track test

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "bugprone-branch-clone"
+#pragma ide diagnostic ignored "hicpp-multiway-paths-covered"
+#pragma ide diagnostic ignored "cert-msc50-cpp"
+
+#include "module/track.h"
+#include <driverlib.h>
+#include <stdint.h>
+#include <msp432p401r.h>
+
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+
+int main(void) {
+    motor_init();
+    track_init();
+    motor_start();
+    track();
+
+}
+
+#pragma clang diagnostic pop
+#pragma clang diagnostic pop
+
