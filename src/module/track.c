eddf6c2d7d5c9bffc209b31ced421da8d29becdf
Spedon <RealSped0n@outlook.com>
2022-12-04 12:31:48 +0800

    optimize params

--- src/module/track.c
+++ src/module/track.c
@@ -1,8 +1,8 @@
 #include "track.h"
 
-#define vert_delay 1500
+#define vert_delay 350
 
-int U1, U2, U3, U4, U5, U6, U7, U8;
+uint8_t U1, U2, U3, U4, U5, U6, U7, U8;
 
 void track_init() {
     GPIO_setAsInputPin(GPIO_PORT_P7,
@@ -12,6 +12,7 @@ void track_init() {
     LED_init();
     CLK_init();
     SysTick_Init();
+    MAP_FPU_enableModule();
 }
 
 void track_scan() {
@@ -29,7 +30,7 @@ void track_scan() {
     //配置为输入，等待电容放电
     GPIO_setAsInputPin(GPIO_PORT_P7,
                        GPIO_PIN0 | GPIO_PIN1 | GPIO_PIN2 | GPIO_PIN3 | GPIO_PIN4 | GPIO_PIN5 | GPIO_PIN6 | GPIO_PIN7);
-    delay_us(500);
+    delay_us(1000);
 
     //读取传感器的值
     U1 = GPIO_getInputPinValue(GPIO_PORT_P7, GPIO_PIN0);
@@ -47,11 +48,11 @@ void track_scan() {
 
 // 90 degrees turn
 void vertical_left() {
-    motor_forward(25);
+    motor_forward(30);
     int i = 0;
     while (!(U1 == 0 && U2 == 0 && U3 == 0 && U4 == 0 && U5 == 0 && U6 == 0 && U7 == 0 && U8 == 0)) {
         track_scan();
-        if (U2 == 1 && U3 == 1 && U4 == 1 && U5 == 1 && U6 == 1 && U8 == 1) {
+        if (U2 == 1 && (U3 == 1 || U6 == 1) && U8 == 1) {
             i = vert_delay;
             break;
         }
@@ -61,20 +62,20 @@ void vertical_left() {
         }
     }
     if (i < vert_delay) {
-        spin_left(70);
+        spin_left(75);
         while (U4 != 1) {
             track_scan();
         }
-        motor_forward(40);
+        motor_forward(10);
     }
 }
 
 void vertical_right() {
-    motor_forward(25);
+    motor_forward(30);
     int i = 0;
     while (!(U1 == 0 && U2 == 0 && U3 == 0 && U4 == 0 && U5 == 0 && U6 == 0 && U7 == 0 && U8 == 0)) {
         track_scan();
-        if (U2 == 1 && U3 == 1 && U4 == 1 && U5 == 1 && U6 == 1 && U8 == 1) {
+        if (U2 == 1 && (U3 == 1 || U6 == 1) && U8 == 1) {
             i = vert_delay;
             break;
         }
@@ -84,11 +85,11 @@ void vertical_right() {
         }
     }
     if (i < vert_delay) {
-        spin_right(70);
+        spin_right(75);
         while (U4 != 1) {
             track_scan();
         }
-        motor_forward(40);
+        motor_forward(10);
     }
 }
 
@@ -97,7 +98,7 @@ void track() {
     // straight
     if (U1 == 0 && U2 == 0 && U3 == 0 && U4 == 1 && U5 == 1 && U6 == 0 && U7 == 0 && U8 == 0) {
         RGB_pure(2);
-        motor_forward(45);
+        motor_forward(55);
     } else if (U1 == 0 && U2 == 0 && U3 == 0 && U4 == 0 && U5 == 0 && U6 == 0 && U7 == 0 && U8 == 0) {
         RED_on();
         spin_right(40);
@@ -108,37 +109,37 @@ void track() {
         RED_off();
     } else if (U3 == 1 && U4 == 1 && U5 == 1 && U6 == 1) {
         RGB_pure(7);
-        motor_forward(100);
+        motor_forward(35);
     } else if (U1 == 0 && U2 == 0 && U3 == 0 && U4 == 1 && U5 == 0 && U6 == 0 && U7 == 0 && U8 == 0) {
         RGB_pure(5);
-        motor_left(40, 1.2);
+        motor_left(45, 1.3);
     } else if (U1 == 0 && U2 == 0 && U3 == 0 && U4 == 0 && U5 == 1 && U6 == 0 && U7 == 0 && U8 == 0) {
         RGB_pure(5);
-        motor_right(40, 1.2);
+        motor_right(45, 1.3);
     } else if (U1 == 0 && U2 == 0 && U3 == 1) {
         RGB_pure(6);
-        motor_left(35, 1.5);
+        motor_left(25, 2.4);
     } else if (U6 == 1 && U7 == 0 && U8 == 0) {
         RGB_pure(6);
-        motor_right(35, 1.5);
+        motor_right(25, 2.4);
     } else if (U7 == 1 && U6 == 0) {
         RGB_pure(4);
-        motor_right(25, 1.9);
+        motor_right(18, 2.7);
     } else if (U2 == 1 && U3 == 0) {
         RGB_pure(4);
-        motor_left(25, 1.9);
+        motor_left(18, 2.7);
     } else if (U1 == 1 && U3 == 1 && U6 == 0 && U7 == 0 && U8 == 0) {
         RGB_pure(3);
         vertical_left();
-    } else if (U6 == 1 && U8 == 1 && U1 == 0 && U2 == 0 && U3 == 0) {
+    } else if (U5 == 1 && U6 == 1 && U1 == 0 && U2 == 0 && U3 == 0) {
         RGB_pure(3);
         vertical_right();
     } else if (U7 == 1) {
         RGB_pure(1);
-        spin_right(40);
+        spin_right(45);
     } else if (U2 == 1) {
         RGB_pure(1);
-        spin_left(40);
+        spin_left(45);
     }
 }
 
